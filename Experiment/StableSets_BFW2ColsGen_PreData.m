f2dd398fe7f527752a4cb519983c3a9131c8f0b3
fengyiliao <rober147456@gmail.com>
2022-01-18 12:26:53 -0800

    2LPCut Version

--- Experiment/StableSets_BFW2ColsGen_PreData.m
+++ Experiment/StableSets_BFW2ColsGen_PreData.m
@@ -4,7 +4,7 @@ function [Obj,elapsedTime]=StableSets_BFW2ColsGen_PreData(Adj,P,At,b,c,K)
     elapsedTime = [];
     
     epsilon = 0.001;
-    Max_Iter = 40; Iter = 1;
+    Max_Iter = 1000; Iter = 1;
     dim_mat = width(Adj);%dimension of design matirx
     
     Obj = [];
@@ -25,20 +25,27 @@ function [Obj,elapsedTime]=StableSets_BFW2ColsGen_PreData(Adj,P,At,b,c,K)
              prob1 = convert_sedumi2mosek(At_new,b_new,c_new,K_new);
              elapse = toc(T1);
              time = [time,elapse];
-         else
-             %T1 = tic;
-            
-             testK = K;
-             testK.s = [NumOfCut];
-             prob2 = convert_sedumi2mosek_modified(At_new(:,[1:K.l+K.f,pre+1:end]),b_new,c_new([1:K.l+K.f,pre+1:end]),testK,pre,NumOfpres);
 
+                 
+         else
              
-             prob1.bardim =[prob1.bardim, prob2.bardim];
-             prob1.bara.subi = [prob1.bara.subi,prob2.bara.subi];
-             prob1.bara.subj = [prob1.bara.subj,prob2.bara.subj];
-             prob1.bara.subk = [prob1.bara.subk,prob2.bara.subk];
-             prob1.bara.subl = [prob1.bara.subl,prob2.bara.subl];
-             prob1.bara.val = [prob1.bara.val,prob2.bara.val];
+             prob1.c = [prob1.c;sparse(2,1)];
+             prob1.blx = [prob1.blx,zeros(1,2)];
+             prob1.a = [prob1.a,-NewV];
+             
+            
+            
+%              testK = K;
+%              testK.s = [NumOfCut];
+%              prob2 = convert_sedumi2mosek_modified(At_new(:,[1:K.l+K.f,pre+1:end]),b_new,c_new([1:K.l+K.f,pre+1:end]),testK,pre,NumOfpres);
+% 
+%              
+%              prob1.bardim =[prob1.bardim, prob2.bardim];
+%              prob1.bara.subi = [prob1.bara.subi,prob2.bara.subi];
+%              prob1.bara.subj = [prob1.bara.subj,prob2.bara.subj];
+%              prob1.bara.subk = [prob1.bara.subk,prob2.bara.subk];
+%              prob1.bara.subl = [prob1.bara.subl,prob2.bara.subl];
+%              prob1.bara.val = [prob1.bara.val,prob2.bara.val];
 
          end
         [rcode1, res1] = mosekopt('minimize info', prob1);
@@ -49,21 +56,24 @@ function [Obj,elapsedTime]=StableSets_BFW2ColsGen_PreData(Adj,P,At,b,c,K)
         
         [V,D] = eig(X);
         [d, I]=sort(diag(D));
-        if d(1)>=0 || abs(d(1))<=epsilon || Iter>=Max_Iter %get PSD 
+        if d(1)>=0 || abs(d(1))<=epsilon || Iter>=Max_Iter || elptime >=600 %get PSD %after 10 mins
             PSD = true;
             break;
         end
         v = V(:,I(1:NumOfCut));
         
-        NewV = kron(v',v');
+        %NewV = kron(v',v');
+        NewV = [vec(v(:,1)*v(:,1)'),vec(v(:,2)*v(:,2)')];
+       
         
         pre = width(At_new);
         NumOfpres = length(K_new.s);
         
-        At_new = [At_new,-NewV'];
-        c_new = [c_new; sparse(NumOfCut^2,1)];
-        K_new.s = [K_new.s,NumOfCut];
-        
+        At_new = [At_new(:,1:K_new.f+K_new.l),-NewV,At_new(:,K_new.f+K_new.l+1:end)];
+        %c_new = [c_new; sparse(NumOfCut^2,1)];
+        c_new = [c_new; sparse(NumOfCut,1)];
+        %K_new.s = [K_new.s,NumOfCut];
+        K_new.l = K_new.l + NumOfCut;
         Iter = Iter +1;
     
     
